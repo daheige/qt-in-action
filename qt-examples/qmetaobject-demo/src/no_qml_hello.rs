a44919d830e4e36390de5c09beea45a0e8512426
daheige <zhuwei313@hotmail.com>
2023-09-10 19:49:34 +0800

    feat: no qml cargo build

---
+++ qt-examples/qmetaobject-demo/src/no_qml_hello.rs
@@ -0,0 +1,35 @@
+use cstr::cstr;
+use qmetaobject::prelude::*;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, QEnum)]
+#[repr(C)]
+enum Options {
+    Foo = 1,
+    Bar = 2,
+    Quaz = 3,
+}
+
+fn main() {
+    // 注册qml名字，版本号，传递给qml文件中rust enum变量
+    qml_register_enum::<Options>(cstr!("qRustCode"), 1, 0, cstr!("Options"));
+
+    let mut engine = QmlEngine::new();
+    engine.load_data(r##"
+        import QtQuick 2.12;
+        import QtQuick.Window 2.12;
+        import qRustCode 1.0;
+
+        Window {
+            visible: true
+            title: "Hello App"
+            height: 480
+            width: 640
+            color: "#e4af79"
+
+            Text {
+                anchors.centerIn: parent
+                text: "Hello! Bar is"+Options.Bar+", Foo is "+Options.Foo+", Quaz is "+Options.Quaz+"."
+            }
+        }"##.into());
+    engine.exec();
+}
